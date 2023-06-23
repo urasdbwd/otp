637ae50236ab44541b473dc7e9f11194e3355411
0xkkl <kaydan@leozc.com>
2023-06-22 21:40:14 -0700

    made it post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import pyotp
 
 app = Flask(__name__)
@@ -7,12 +7,8 @@ app = Flask(__name__)
 # The secret key should be shared between the user and the server
 secret_key = pyotp.random_base32()
 
-
-from flask import jsonify
-
-
-@app.route('/generate/<secret_key>', methods=['POST','GET'])
-def generate_otp(secret_key):
+@app.route('/generate/', methods=['POST'])
+def generate_otp():
     # Generate a new OTP for the current time
     totp = pyotp.TOTP(secret_key)
     otp = totp.now()
@@ -22,8 +18,10 @@ def generate_otp(secret_key):
     response.headers['Access-Control-Allow-Origin'] = '*'
 
     return response
+
 @app.route('/')
 def gen():
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
